901e99ccf78506428a900779f05d2fe0c185de97
Damian Bielecki <bielik20@gmail.com>
2019-04-29 18:32:30 +0200

    Add types watch

--- rollup.config.ts
+++ rollup.config.ts
@@ -5,52 +5,67 @@ import sourceMaps from 'rollup-plugin-sourcemaps'
 import camelCase from 'lodash.camelcase'
 import typescript from 'rollup-plugin-typescript2'
 import json from 'rollup-plugin-json'
+import { resolvePaths } from 'tscpaths'
 
 const pkg = require('./package.json')
 
 const libraryName = 'tree-shakeable-lib'
 
-function base(declaration = false) {
+function tscpaths() {
   return {
-    input: `src/index.ts`,
-    watch: {
-      include: 'src/**'
-    },
-    plugins: [
-      // Allow json resolution
-      json(),
-      // Compile TypeScript files
-      typescript({
-        check: false,
-        useTsconfigDeclarationDir: true,
-        tsconfigOverride: { compilerOptions: { declaration } }
-      }),
-      // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-      commonjs(),
-      // Allow node_modules resolution, so you can use 'external' to control
-      // which external modules to include in the bundle
-      // https://github.com/rollup/rollup-plugin-node-resolve#usage
-      resolve(),
-
-      // Resolve source maps to the original source
-      sourceMaps()
-    ]
+    onwrite() {
+      resolvePaths({
+        project: 'tsconfig.json',
+        src: './src',
+        out: './dist/types'
+      })
+    }
   }
 }
 
+const common = {
+  input: `src/index.ts`,
+  watch: {
+    include: 'src/**'
+  }
+}
+
+const commonPlugins = [
+  // Allow json resolution
+  json(),
+  // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
+  commonjs(),
+  // Allow node_modules resolution, so you can use 'external' to control
+  // which external modules to include in the bundle
+  // https://github.com/rollup/rollup-plugin-node-resolve#usage
+  resolve(),
+  // Resolve source maps to the original source
+  sourceMaps()
+]
+
 const targets = {
   es: {
-    ...base(true),
+    ...common,
     output: [{ file: pkg.module, format: 'es', sourcemap: true }],
     // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
-    external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})]
+    external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
+    plugins: [
+      ...commonPlugins,
+      typescript({
+        check: false,
+        useTsconfigDeclarationDir: true,
+        tsconfigOverride: { compilerOptions: { declaration: true } }
+      }),
+      tscpaths()
+    ]
   },
+
   umd: {
-    ...base(),
+    ...common,
     output: [{ file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true }],
     // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
     external: [],
-    plugins: [...base().plugins, uglify()]
+    plugins: [...commonPlugins, typescript({ check: false }), uglify()]
   }
 }
 
