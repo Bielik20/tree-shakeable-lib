633c87443d5605246865bce2fc879a2c9b37e746
Damian Bielecki <bielik20@gmail.com>
2019-04-29 17:20:11 +0200

    Add suppress TypeScript errors

--- rollup.config.ts
+++ rollup.config.ts
@@ -10,41 +10,47 @@ const pkg = require('./package.json')
 
 const libraryName = 'tree-shakeable-lib'
 
-const base = {
-  input: `src/index.ts`,
-  watch: {
-    include: 'src/**'
-  },
-  plugins: [
-    // Allow json resolution
-    json(),
-    // Compile TypeScript files
-    typescript(),
-    // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-    commonjs(),
-    // Allow node_modules resolution, so you can use 'external' to control
-    // which external modules to include in the bundle
-    // https://github.com/rollup/rollup-plugin-node-resolve#usage
-    resolve(),
+function base(declaration = false) {
+  return {
+    input: `src/index.ts`,
+    watch: {
+      include: 'src/**'
+    },
+    plugins: [
+      // Allow json resolution
+      json(),
+      // Compile TypeScript files
+      typescript({
+        check: false,
+        useTsconfigDeclarationDir: true,
+        tsconfigOverride: { compilerOptions: { declaration } }
+      }),
+      // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
+      commonjs(),
+      // Allow node_modules resolution, so you can use 'external' to control
+      // which external modules to include in the bundle
+      // https://github.com/rollup/rollup-plugin-node-resolve#usage
+      resolve(),
 
-    // Resolve source maps to the original source
-    sourceMaps()
-  ]
+      // Resolve source maps to the original source
+      sourceMaps()
+    ]
+  }
 }
 
 const targets = {
   es: {
-    ...base,
+    ...base(true),
     output: [{ file: pkg.module, format: 'es', sourcemap: true }],
     // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
     external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})]
   },
   umd: {
-    ...base,
+    ...base(),
     output: [{ file: pkg.main, name: camelCase(libraryName), format: 'umd', sourcemap: true }],
     // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
     external: [],
-    plugins: [...base.plugins, uglify()]
+    plugins: [...base().plugins, uglify()]
   }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 // Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
 // import "core-js/fn/array.find"
 // ...
-export class DummyClass {}
+export class DummyClass {
+  constructor() {
+    this.name = 'bielik'
+  }
+}
 
 export * from '@ad-engine'
